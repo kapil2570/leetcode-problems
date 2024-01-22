8ae172e105b55cb8ac313b596fe907a887f4be68
Kapil Sahu <kapilupcl@gmail.com>
2024-01-22 15:06:32 +0530

    Time: 32 ms (47.64%) | Memory: 32.5 MB (12.63%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        unordered_set<int>store;
         int n=nums.size(), sum=0;
         vector<int>res;
-        for(int i=0 ; i<n-1 ; i++)
-        {
+        for(int i=0 ; i<n ; i++)
             sum+=nums[i];
-            if(nums[i]==nums[i+1])
+        for(auto num:nums)
+        {
+            if(store.find(num)!=store.end())
             {
-                res.push_back(nums[i]);
+                int dup=num;
+                res.push_back(dup);
+                break;
             }
+            else
+                store.insert(num);
         }
-        sum+=nums[n-1];
         int num = (n*(n+1))/2 - sum + res[0];
         res.push_back(num);
         return res;
