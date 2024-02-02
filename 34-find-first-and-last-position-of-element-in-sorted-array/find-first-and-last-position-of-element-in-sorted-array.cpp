7813def9a6f7e24e6f94998714feed345fd567c8
Kapil Sahu <kapilupcl@gmail.com>
2024-02-03 00:48:48 +0530

    Time: 3 ms (92.39%) | Memory: 16 MB (12.34%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        int low=0, high=n-1, mid;
-        int ans1=-1, ans2=-1;
-        while(low<=high)
-        {
-            mid = (low+high)/2;
-            if(nums[mid]==target)
-            {
-                ans1=mid;
-                high=mid-1;
+        int low=0, high=nums.size()-1;
+        vector<int>res(2,-1);
+        if(nums.size()==0)
+            return res;
+        while(low<high) {
+            int mid=(low+high)/2;
+            if(nums[mid]>=target) {
+                high=mid;
             }
-            else if(nums[mid]<target)
+            else
                 low=mid+1;
+        }
+        if(nums[low]!=target)
+            return res;
+        else
+            res[0]=low;
+        
+        high=nums.size()-1;
+        while(low<high) {
+            int mid=(low+high)/2+1;
+            if(nums[mid]<=target)
+                low=mid;
             else
                 high=mid-1;
         }
-        low=0, high=n-1;
-        while(low<=high)
-        {
-            mid = (low+high)/2;
-            if(nums[mid]==target)
-            {
-                ans2=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]<target)
-                low=mid+1;
-            else
-                high=mid-1;
-        }
-        vector<int>ans{ans1,ans2};
-        return ans;
+        res[1]=high;
+        return res;
     }
 };
