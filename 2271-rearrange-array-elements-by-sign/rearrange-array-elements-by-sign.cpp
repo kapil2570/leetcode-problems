4ae671a5f9841576c5944960a4a8de4666479d98
Kapil Sahu <kapilupcl@gmail.com>
2024-02-14 14:31:27 +0530

    Time: 140 ms (85.51%) | Memory: 126.2 MB (65.68%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -4,17 +4,14 @@ public:
         int n = nums.size();
         vector<int>res(n,0);
         int pos=0, neg=1;
-        for(auto num: nums)
-        {
-            if(num>0)
-            {
+        for(auto num : nums) {
+            if(num>0) {
                 res[pos] = num;
-                pos += 2;
+                pos+=2;
             }
-            else
-            {
+            else {
                 res[neg] = num;
-                neg += 2;
+                neg+=2;
             }
         }
         return res;
