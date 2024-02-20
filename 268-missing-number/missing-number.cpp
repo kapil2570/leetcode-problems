a96308682e9a10740a82549d7f985c365ebf1311
Kapil Sahu <kapilupcl@gmail.com>
2024-02-20 20:15:04 +0530

    Time: 10 ms (88.91%) | Memory: 20.4 MB (32.85%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int len = nums.size();
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        int maxm = *max_element(nums.begin(),nums.end());
-        if(len==maxm+1)
-            return maxm+1;
-        int reqSum = (maxm*(maxm+1))/2;
-        return reqSum-sum;
+        int sum = (len*(len+1))/2;
+        for(auto num : nums) {
+            sum -= num;
+        }
+        return sum;
     }
 };
