4a24d892c958e2528c5e38310936487a606a1990
Kapil Sahu <kapilupcl@gmail.com>
2024-03-01 12:47:51 +0530

    Time: 3 ms (77.44%) | Memory: 8.2 MB (63.14%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -9,11 +9,11 @@ public:
             else
                 ones++;
         }
-        for(int i=0 ; i<ones-1 ; i++) {
-            s[i] = '1';
-        }
-        for(int i=ones-1 ; i<n-1 ; i++) {
-            s[i] = '0';
+        for(int i=0 ; i<n-1 ; i++) {
+            if(i<ones-1)
+                s[i] = '1';
+            else
+                s[i] = '0';
         }
         s[n-1] = '1';
         return s;
