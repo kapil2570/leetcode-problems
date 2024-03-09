95af3100ffc6003f29d617769c6c065ed2230535
Kapil Sahu <kapilupcl@gmail.com>
2024-03-10 00:59:57 +0530

    Time: 3 ms (42.04%) | Memory: 7.5 MB (49.97%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto ch:s) {
+            if(!st.empty() && ((st.top()=='(' && ch==')') || (st.top()=='{' && ch=='}')
+                    || (st.top()=='[' && ch==']'))) {
+                        st.pop();
+            }
+            else
+                st.push(ch);
+        }
+        return st.empty();
+    }
+};
