f489dfb38cee352d301b575b9804312e46de7b8c
Kapil Sahu <kapilupcl@gmail.com>
2024-03-10 14:41:40 +0530

    Time: 3 ms (83.79%) | Memory: 12.5 MB (51.34%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    bool search(vector<int>&nums , int num) {
+        int low=0, high=nums.size()-1;
+        while(low<=high) {
+            int mid = (low+high)/2;
+            if(nums[mid]==num)
+                return true;
+            else if(nums[mid]<num)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return false;
+    }
+
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         if(nums1.size() > nums2.size())
             return intersection(nums2,nums1);
@@ -7,10 +21,14 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
         vector<int>res;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+
         for(auto num:nums1) {
-            if(find(res.begin(),res.end(),num)==res.end() &&
-                 find(nums2.begin(),nums2.end(),num)!=nums2.end())
-                 res.push_back(num);
+            if(find(res.begin(),res.end(),num)==res.end()) {
+                if(search(nums2, num))
+                    res.push_back(num);
+            }
         }
         return res;
     }
