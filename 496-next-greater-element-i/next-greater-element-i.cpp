0898769d9ad7b099a36017fb592527a2fa800a4f
Kapil Sahu <kapilupcl@gmail.com>
2024-03-12 01:44:57 +0530

    Time: 8 ms (35.41%) | Memory: 12 MB (39.23%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -5,47 +5,17 @@ public:
         int n = nums2.size();
         unordered_map<int,int>mpp;
 
-        for(int i=0 ; i<nums1.size() ; i++) {
-            int num = nums1[i];
-            for(int j=0 ; j<nums2.size() ; j++) {
-                if(num==nums2[j]) {
-                    mpp[num] = j;
-                    break;
-                }
+        for(auto num : nums2) {
+            while(!st.empty() && st.top()<num) {
+                mpp[st.top()] = num;
+                st.pop();
             }
+            st.push(num);
         }
-
-        for(int i=n-1 ; i>=0 ; i--) {
-            if(st.empty()) {
-                st.push(nums2[i]);
-                nums2[i] = -1;
-                continue;
-            }
-            if(st.top()>nums2[i]) {
-                int num = nums2[i];
-                nums2[i] = st.top();
-                st.push(num);
-            }
-            else {
-                while(!st.empty() && st.top()<=nums2[i]) {
-                    st.pop();
-                }
-                if(st.empty()) {
-                    int num = nums2[i];
-                    nums2[i] = -1;
-                    st.push(num);
-                }
-                else {
-                    int num = nums2[i];
-                    nums2[i] = st.top();
-                    st.push(num);
-                }
-            }
-        }
-
         vector<int>res;
         for(auto num:nums1) {
-            res.push_back(nums2[mpp[num]]);
+            int elem = mpp.count(num) ? mpp[num] : -1;
+            res.push_back(elem);
         }
         return res;
     }
