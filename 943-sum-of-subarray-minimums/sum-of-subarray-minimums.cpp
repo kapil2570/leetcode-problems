b9cbd2a6525bdd78c6360433ee2534a3a61b3ed7
Kapil Sahu <kapilupcl@gmail.com>
2024-03-14 01:55:22 +0530

    Time: 59 ms (69.24%) | Memory: 48.3 MB (9.97%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-  int sumSubarrayMins(vector<int>& A) {
-    stack<pair<int, int>> in_stk_p, in_stk_n;
-    // left is for the distance to previous less element
-    // right is for the distance to next less element
-    vector<long long> left(A.size()), right(A.size());
-		
-    //initialize
-    for(int i = 0; i < A.size(); i++) left[i] =  i + 1;
-    for(int i = 0; i < A.size(); i++) right[i] = A.size() - i;
-		
-    for(int i = 0; i < A.size(); i++){
-      // for previous less
-      while(!in_stk_p.empty() && in_stk_p.top().first > A[i]) in_stk_p.pop();
-      left[i] = in_stk_p.empty()? i + 1: i - in_stk_p.top().second;
-      in_stk_p.push({A[i],i});
-			
-      // for next less
-      while(!in_stk_n.empty() && in_stk_n.top().first > A[i]){
-        auto x = in_stk_n.top();in_stk_n.pop();
-        right[x.second] = i - x.second;
-      }
-      in_stk_n.push({A[i], i});
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<long long>prev_elem(n), next_elem(n);
+        for(long long i=0 ; i<n ; i++) {
+            prev_elem[i] = i;
+            next_elem[i] = n-i-1;
+        }
+        stack<int>st;
+        for(int i=0 ; i<n ; i++) {
+            while(!st.empty() && arr[i]<=arr[st.top()]) {
+                next_elem[st.top()] = i-st.top()-1;
+                st.pop();
+            }
+            st.push(i);
+        }
+        while(!st.empty()) {
+            st.pop();
+        }
+        for(int i=n-1 ; i>=0 ; i--) {
+            while(!st.empty() && arr[i]<arr[st.top()]) {
+                prev_elem[st.top()] = st.top()-i-1;
+                st.pop();
+            }
+            st.push(i);
+        }
+        long long ans = 0;
+        int mod = 1e9+7;
+        for(int i=0 ; i<n ; i++) {
+            ans = (ans + (arr[i]*(prev_elem[i]+1)*(next_elem[i]+1))%mod)%mod;
+        }
+        return ans;
     }
-
-    long long ans = 0, mod = 1e9 +7;
-    for(int i = 0; i < A.size(); i++){
-      long long num = A[i]*left[i]*right[i];
-      ans = (ans + num)%mod;
-    }
-    return ans;
-  }
 };
