2205291f387025c703c5360e243d73b95a8e986e
Kapil Sahu <kapilupcl@gmail.com>
2024-03-21 12:42:08 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (67.59%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -8,32 +8,19 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-// **********************Iterative Approach*************************
-// class Solution {
-// public:
-//     ListNode* reverseList(ListNode* head) {
-//         ListNode *prev = NULL, *temp = head, *front = head;
-//         while(temp) {
-//             front = temp->next;
-//             temp->next = prev;
-//             prev = temp;
-//             temp = front;
-//         }
-//         return prev;
-//     }
-// };
-
-// **********************Recursive Approach*************************
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         if(head==NULL || head->next==NULL)
             return head;
-        ListNode *newHead = reverseList(head->next);
-        ListNode *front = head->next;
-        front->next = head;
-        head->next = NULL;
-        return newHead;
+        ListNode *prev = NULL;
+        ListNode *temp = head, *front;
+        while(temp) {
+            front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
     }
 };
