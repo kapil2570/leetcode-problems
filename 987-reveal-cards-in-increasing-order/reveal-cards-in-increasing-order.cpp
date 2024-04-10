540933a7a7e39e3dedd8210a47b3d2cb2777b086
Kapil Sahu <kapilupcl@gmail.com>
2024-04-10 11:50:29 +0530

    Time: 8 ms (18.53%) | Memory: 11.8 MB (5.76%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
-        deque<int>dq;
-        sort(deck.begin(),deck.end());
-        int n = deck.size();
-        for(int i=n-1 ; i>=0 ; i--) {
-            if(!dq.empty()) {
-                int num = dq.back();
-                dq.pop_back();
-                dq.push_front(num);
-            }
-            dq.push_front(deck[i]);
-        }
+        // deque<int>dq;
+        // sort(deck.begin(),deck.end());
+        // int n = deck.size();
+        // for(int i=n-1 ; i>=0 ; i--) {
+        //     if(!dq.empty()) {
+        //         int num = dq.back();
+        //         dq.pop_back();
+        //         dq.push_front(num);
+        //     }
+        //     dq.push_front(deck[i]);
+        // }
+        // vector<int>res;
+        // while(!dq.empty()) {
+        //     res.push_back(dq.front());
+        //     dq.pop_front();
+        // }
+        // return res;
+
         vector<int>res;
-        while(!dq.empty()) {
-            res.push_back(dq.front());
-            dq.pop_front();
+        sort(deck.begin(),deck.end(),greater<int>());
+        int n = deck.size();
+        res.push_back(deck[0]);
+        for(int i=1 ; i<n ; i++) {
+            int last = res.back();
+            res.pop_back();
+            res.insert(res.begin(),last);
+            res.insert(res.begin(),deck[i]);
         }
         return res;
     }
