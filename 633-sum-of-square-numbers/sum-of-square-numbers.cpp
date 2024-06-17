267d57b529f8ae6b4e384bfab38a444dbf4ade11
Kapil Sahu <kapilupcl@gmail.com>
2024-06-17 12:49:03 +0530

    Time: 6 ms (27.80%) | Memory: 7 MB (61.35%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        int low=0, high=ceil(sqrt(c));
-        while(low<=high) {
-            long long sum = pow(low,2) + pow(high,2);
-            if(sum == c)
+        // int low=0, high=ceil(sqrt(c));
+        // while(low<=high) {
+        //     long long sum = pow(low,2) + pow(high,2);
+        //     if(sum == c)
+        //         return true;
+        //     else if(sum > c)
+        //         high--;
+        //     else
+        //         low++;
+        // }
+        // return false;
+
+        for(int a=0 ; a<=sqrt(c) ; a++) {
+            int b = sqrt(c - a*a);
+            if(b*b == (c - a*a))
                 return true;
-            else if(sum > c)
-                high--;
-            else
-                low++;
         }
         return false;
     }
